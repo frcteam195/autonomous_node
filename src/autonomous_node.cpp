ef305468101aeea0dee59fb62d5578a1207c354d
sulicat <suli.barakat@gmail.com>
2022-01-16 19:58:39 -0500

    start

--- src/autonomous_node.cpp
+++ src/autonomous_node.cpp
@@ -5,26 +5,72 @@
 #include <string>
 #include <mutex>
 
+#include <../../action_processor_node/include/helper/action_helper.hpp>
+
+#define RATE (100)
+
 ros::NodeHandle* node;
+ActionHelper* action_helper;
+
+enum STATE
+{
+    IDLE,
+    MOVING_FORWARD
+};
+
+enum STATE state;
+int timer = 0;
+
+void move_forward()
+{
+    std::cout << "move fprward\n";
+    state = STATE::IDLE;
+    timer = 0;
+}
 
 int main(int argc, char **argv)
 {
-	/**
-	 * The ros::init() function needs to see argc and argv so that it can perform
-	 * any ROS arguments and name remapping that were provided at the command line.
-	 * For programmatic remappings you can use a different version of init() which takes
-	 * remappings directly, but for most command-line programs, passing argc and argv is
-	 * the easiest way to do it.  The third argument to init() is the name of the node.
-	 *
-	 * You must call one of the versions of ros::init() before using any other
-	 * part of the ROS system.
-	 */
+
 	ros::init(argc, argv, "autonomous_node");
-
 	ros::NodeHandle n;
-
+    ros::Rate rate(RATE);
 	node = &n;
 
-	ros::spin();
+    // listen to move actions
+    action_helper = new ActionHelper(node);
+
+    state = STATE::IDLE;
+
+    while( ros::ok() )
+    {
+        //action_helper->step();
+
+        if( state == STATE::MOVING_FORWARD )
+        {
+            timer += 1;
+
+            if( timer > 500 )
+            {
+                action_helper->update_action( "MoveForward",
+                                              ActionHelper::ACTION_STATUS::COMPLETE );
+               state = STATE::IDLE;
+               std::cout << "asdasdasd\n\n";
+            }
+        }
+        else
+        {
+            if( action_helper->check_action( "MoveForward" ) )
+            {
+                action_helper->update_action( "MoveForward",
+                                              ActionHelper::ACTION_STATUS::EXECUTING );
+                move_forward();
+                state = STATE::MOVING_FORWARD;
+            }
+        }
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
 	return 0;
 }
