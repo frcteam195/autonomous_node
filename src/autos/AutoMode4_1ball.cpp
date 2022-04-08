435e51667ed87bbfad6aa8681aba9d2986e72ed2
= <=>
2022-04-08 18:03:13 +0000

    Update auto

--- src/autos/AutoMode4_1ball.cpp
+++ src/autos/AutoMode4_1ball.cpp
@@ -10,6 +10,7 @@ hmi_agent_node::HMI_Signals AutoMode4_1ball::stepStateMachine(bool trajRunning, 
     hmi_agent_node::HMI_Signals autoHMISignals;
 
 	static ros::Time time_state_entered = ros::Time::now();
+    static int intake_counter = 0;
 	if(mNextState != mAutoState)
 	{
         ROS_INFO("Running state: %d", (int)mAutoState);
@@ -37,16 +38,21 @@ hmi_agent_node::HMI_Signals AutoMode4_1ball::stepStateMachine(bool trajRunning, 
         }
         case AutoMode4States::SHOOT_1:
         {
-            autoHMISignals.intake_rollers = true;
-            autoHMISignals.allow_shoot = true;
-            if ((ros::Time::now() - time_state_entered) > ros::Duration(3))
+            autoHMISignals.retract_intake = true;
+            if (intake_counter++ < 5)
             {
-                mNextState = AutoMode4States::END;
+                autoHMISignals.intake_rollers = true;
+            }
+            autoHMISignals.allow_shoot = true;
+            if ((ros::Time::now() - time_state_entered) > ros::Duration(2))
+            {
+                mNextState = AutoMode4States::BEGIN_PATH_2;
             }
             break;
         }
         case AutoMode4States::BEGIN_PATH_2:
         {
+            autoHMISignals.retract_intake = true;
             AutonomousHelper::getInstance().drive_trajectory(6);
             ROS_INFO("Started trajectory id: %d", 6);
             mNextState = AutoMode4States::DRIVE_PATH_2;
@@ -54,6 +60,8 @@ hmi_agent_node::HMI_Signals AutoMode4_1ball::stepStateMachine(bool trajRunning, 
         }
         case AutoMode4States::DRIVE_PATH_2:
         {
+            autoHMISignals.retract_intake = true;
+            autoHMISignals.intake_rollers = true;
             if (!trajRunning && trajCompleted)
             {
                 mNextState = AutoMode4States::END;
