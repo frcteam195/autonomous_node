c506dbc72a0c62c284676357ea5ca7f7a4c90cbf
Mac #1 <frc195programming@gmail.com>
2022-07-09 16:09:34 -0400

    Added intake/outake functions to Hangar auto.

--- src/autos/Auto1BallHangar.cpp
+++ src/autos/Auto1BallHangar.cpp
@@ -23,12 +23,14 @@ hmi_agent_node::HMI_Signals Auto1BallHangar::stepStateMachine(bool trajRunning, 
     {
         case AutoStates::BEGIN_INITIAL_PATH:
         {
+            autoHMISignals.retract_intake = false;
             AutonomousHelper::getInstance().drive_trajectory(50);
             mNextState = AutoStates::DRIVE_INITIAL_PATH;
             break;
         }
         case AutoStates::DRIVE_INITIAL_PATH:
-        {   
+        {
+            autoHMISignals.retract_intake = false;
             if (!trajRunning && trajCompleted)
             {
                 mNextState = AutoStates::SHOOT;
@@ -37,48 +39,55 @@ hmi_agent_node::HMI_Signals Auto1BallHangar::stepStateMachine(bool trajRunning, 
         }
         case AutoStates::SHOOT:
         {
+            autoHMISignals.retract_intake = false;
             autoHMISignals.allow_shoot = true;
             if ((ros::Time::now() - time_state_entered) > ros::Duration(2))
             {
-                mNextState = AutoStates::END;
+                mNextState = AutoStates::INTAKE_OPPONENT_BALL;
             }
             break;
         }
         case AutoStates::INTAKE_OPPONENT_BALL:
         {
+            autoHMISignals.intake_do_not_eject = false;
             autoHMISignals.intake_rollers = true;
 
             if ((ros::Time::now() - time_state_entered) > ros::Duration(0.5))
             {
-                mNextState = AutoStates::END;
+                mNextState = AutoStates::BEGIN_HANGAR_PATH;
             }
             break;
         }
         case AutoStates::BEGIN_HANGAR_PATH:
         {
+            autoHMISignals.intake_do_not_eject = false;
             autoHMISignals.intake_rollers = true;
+
             AutonomousHelper::getInstance().drive_trajectory(51);
-            mNextState = AutoStates::DRIVE_INITIAL_PATH;
+            mNextState = AutoStates::DRIVE_HANGAR_PATH;
             break;
         }
         case AutoStates::DRIVE_HANGAR_PATH:
         {   
+            autoHMISignals.intake_do_not_eject = false;
             autoHMISignals.intake_rollers = true;
 
             if (!trajRunning && trajCompleted)
             {
-                mNextState = AutoStates::SHOOT;
+                mNextState = AutoStates::BEGIN_STASH_PATH;
             }
             break;
         }
         case AutoStates::BEGIN_STASH_PATH:
         {
+            autoHMISignals.intake_do_not_eject = false;
             AutonomousHelper::getInstance().drive_trajectory(52);
-            mNextState = AutoStates::DRIVE_INITIAL_PATH;
+            mNextState = AutoStates::DRIVE_STASH_PATH;
             break;
         }
         case AutoStates::DRIVE_STASH_PATH:
         {  
+            autoHMISignals.intake_do_not_eject = false;
             if (!trajRunning && trajCompleted)
             {
                 mNextState = AutoStates::STASH;
@@ -87,6 +96,7 @@ hmi_agent_node::HMI_Signals Auto1BallHangar::stepStateMachine(bool trajRunning, 
         }
         case AutoStates::STASH:
         {
+            autoHMISignals.manual_outake_back = true;
             if ((ros::Time::now() - time_state_entered) > ros::Duration(2))
             {
                 mNextState = AutoStates::END;
